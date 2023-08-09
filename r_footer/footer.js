e7603c81ccae430a2815d667daf32d648d50b380
Rensi <rensidudhat2411@gmail.com>
2023-08-09 12:37:41 +0530

    l

---
+++ r_footer/footer.js
@@ -0,0 +1,80 @@
+function footer (){
+    return ` <div class="fo">
+    <div class="fo-1">
+        <div class="fo-1a">
+            <img src="https://i.pinimg.com/originals/db/12/20/db122066a33621a40e3783f69e2477d2.jpg" alt="logo">
+        </div>
+        <div class="fo-1b">
+            <i class="fa-brands fa-youtube" style="color: #f4f5f6;"></i>
+            <i class="fa-brands fa-square-facebook" style="color: #f5f6f6;"></i>
+            <i class="fa-brands fa-linkedin" style="color: #f1f4f8;"></i>
+            <i class="fa-brands fa-instagram" style="color: #f7f9fc;"></i>
+        </div>
+    </div>
+    <div class="fo-2">
+        <div class="fo-2a">
+            <h3>MOUNTAIN</h3>
+            <p>Cross Country</p>
+            <p>Downhill</p>
+            <p>Dirt Jump</p>
+            <p>Frames</p>
+        </div>
+        <div class="fo-2b">
+            <h3>ROAD</h3>
+            <p>Performance</p>
+            <p>Gravel</p>
+            <p>Cyclocross</p>
+            <p>Triathlon</p>
+        </div>
+        <div class="fo-2c">
+            <h3>ACTIVE</h3>
+            <p>Fitness</p>
+            <p>Transport</p>
+            <p>Comfort</p>
+        </div>
+        <div class="fo-2d">
+            <h3>ELECTRIC</h3>
+            <p>Road</p>
+            <p>Mountain</p>
+            <p>Active</p>
+        </div>
+        <div class="fo-2e">
+            <h3>KIDS</h3>
+            <p>Toddler</p>
+            <p>Little Kids</p>
+            <p>Big Kids</p>
+        </div>
+        <div class="fo-2f">
+            <h3>CUSTOMERS</h3>
+            <p>About us</p>
+            <p>Delivery</p>
+            <p>Payment</p>
+            <p>Contacts</p>
+        </div>
+        <div class="fo-2g">
+            <h3>Support</h3>
+            <p>Support Center</p>
+            <p>Warranty</p>
+            <p>Bike Registration</p>
+            <p>Shipping&Delivery</p>
+        </div>
+    </div>
+    <div class="fo-3">
+        <div class="fo-3a">
+            <h3>NEWSLETTER</h3>
+            <div class="fo-3a1">
+                <input type="text"placeholder="Join our newsletter">
+                <button>JOIN</button>
+            </div>
+        </div>
+        <div class="fo-3b">
+            <p>By submitting your email address you agree to the <a href="#">Terms & Conditions</a></p>
+        </div>
+    </div>
+</div>
+<div class="fo-4">
+    <i class="fa-regular fa-copyright" style="color: #ecedef;"></i>
+    <h4>1997 - 2022. Specialized Bicycle Components, inc.All Rights Reserved</h4>
+</div>`
+}
+export default footer;
---
+++ r_navbar/navbar.js
@@ -0,0 +1,35 @@
+function footer (){
+    return `<div class="nav-a">
+    <div class="nav-a1">
+        <img src="https://i.pinimg.com/originals/db/12/20/db122066a33621a40e3783f69e2477d2.jpg" alt="logo">
+    </div>
+    <div class="nav-a2">
+        <p style="margin-left: 100px;">MOUNTAIN</p>
+        <p>ROAD</p>
+        <p>ACTIVE</p>
+        <p>ELECTRIC</p>
+        <p style="margin-right: 100px;">KIDS</p>
+    </div>
+    <div class="nav-a3">
+        <input type="text"placeholder="Search">
+        <i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i>
+    </div>
+    <div class="nav-a4">
+        <i class="fa-regular fa-heart" style="color: #ffffff;margin-left: 100px;"></i>
+        <i class="fa-solid fa-repeat" style="color: #ffffff;"></i>
+        <i class="fa-solid fa-cart-shopping" style="color: #ffffff;"></i>
+        <i class="fa-regular fa-circle-user" style="color: #ffffff;margin-right: 70px;"></i>
+    </div>
+</div>
+<hr>
+<div class="nav-b">
+    <div class="nav-b1">
+        <i class="fa-solid fa-location-dot" style="color: #e9a920;"></i>
+        <p>About us</p>
+        <p>Delivery</p>
+        <p>Payment</p>
+        <p style="margin-right: 30px;">Contact</p>
+    </div>
+</div>`
+}
+export default footer;
