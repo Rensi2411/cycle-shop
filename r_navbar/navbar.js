bb0766131bef6eabdadde68c7a5a7b74fbb5b1e0
Rensi <rensidudhat2411@gmail.com>
2023-08-15 18:54:27 +0530

    ok

--- r_navbar/navbar.js
+++ r_navbar/navbar.js
@@ -141,7 +141,7 @@ function navbar (){
           
         <i class="fa-solid fa-magnifying-glass" style="color: #ffffff; margin: 0px 0px 0px 60px;"></i>
           <input type="search" placeholder="Search" >
-          <a href="login.html" id="login_text" >Login</a>
+          <a href="signup.html" id="login_text" >Login</a>
           <span id="username_text"></span>
           
           <button id="logout_text">Logout</button>
@@ -171,31 +171,3 @@ function navbar (){
 export default navbar;
 
 
-// let Namearr = JSON.parse(localStorage.getItem("username"));
-
-// function handleLogout() {
-
-//   // Update the UI to show the login link and hide the username and logout button
-//   document.getElementById("login_text").style.display = "inline";
-//   document.getElementById("username_text").innerText = "";
-//   document.getElementById("username_text").style.display = "none";
-//   document.getElementById("logout_text").style.display = "none";
-// }
-
-// if (Namearr && Namearr.length > 0) {
-//   // User is logged in
-//   let loggedInUser = Namearr[0].name;
-//   document.getElementById("login_text").style.display = "none";
-//   document.getElementById("username_text").innerText = loggedInUser;
-//   document.getElementById("username_text").style.display = "inline";
-//   document.getElementById("logout_text").style.display = "inline";
-
-//   // Add event listener to the logout button
-//   document.getElementById("logout_text").addEventListener("click", handleLogout);
-// } else {
-//   // User is not logged in
-//   document.getElementById("login_text").style.display = "inline";
-//   document.getElementById("username_text").innerText = "";
-//   document.getElementById("username_text").style.display = "none";
-//   document.getElementById("logout_text").style.display = "none";
-// }
